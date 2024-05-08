9c676d3eacb5430910f7c4a8f6f105fb7bc36597
karra-rani <144821092+karra-rani@users.noreply.github.com>
2024-05-08 07:13:52 +0530

    Add files via upload

---
+++ fundamentals.js
@@ -0,0 +1,16 @@
+console.log("hello world");
+let a;
+a=1;
+console.log(a);
+let b = 4.5;
+let c = "rani"
+console.log(typeof(b));
+console.log(typeof(c));
+let d = 2;
+let e = String (d);
+let f = Boolean(d);
+console.log(d);
+console.log(e);
+console.log(f);
+let g = a + "" +b;
+console.log(g);
